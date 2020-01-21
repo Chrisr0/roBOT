52fe2a442c7598c3fbd7c7d3898b8578f53e38eb
Chrisr0 <36136394+Chrisr0@users.noreply.github.com>
2020-01-21 23:14:01 +0100

    Update cat.js

--- modules/cat.js
+++ modules/cat.js
@@ -4,7 +4,7 @@ var catApiUrl = 'https://api.thecatapi.com/v1/images/search';
 
 exports.getUrl = function() {
     var stream;
-    stream = request(catApiUrl);
+    stream = request(catApiUrl).pipe();
     const chunks = []
     return new Promise((resolve, reject) => {
     stream.on('data', chunk => chunks.push(chunk))
