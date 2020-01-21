5659dd0f709ad8d8600f6345eb765aa786e4cef3
Chrisr0 <36136394+Chrisr0@users.noreply.github.com>
2020-01-21 23:50:28 +0100

    Separated greeting message to greet module

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 
 const CatModule = require('./modules/cat.js');
+const GreetModule = require('./modules/greeting.js')
 
 console.log('Start');
 
@@ -32,15 +33,7 @@ client.on('message', message => {
 });
 
 client.on('guildMemberAdd', member => {
-
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'ogólny');
-
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-
-  // Send the message, mentioning the member
-  channel.send(`Witaj ${member}, życzymy miłej gry :tada::hugging: !`);
+    GreetModule.send(member);
 });
 
  
