f844a0676b7164ca2e983db05befc0d890f63a4c
Chrisr0 <36136394+Chrisr0@users.noreply.github.com>
2020-01-21 21:57:49 +0100

    Update cat.js

--- modules/cat.js
+++ modules/cat.js
@@ -1,1 +1,15 @@
+var catApiUrl = 'https://api.thecatapi.com/v1/images/search';
 
+exports.getUrl = function() {
+    var url;
+    var response = fetch(catApiUrl);
+
+    if(response.ok){
+        url = response.json()[0].url;
+    }else{
+        url = 'error';
+        console.log(response.status);
+    }
+
+    return url;
+}
