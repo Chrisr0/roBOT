deb5a075767168b57f800f683d2bbe4e7543f04c
Chrisr0 <36136394+Chrisr0@users.noreply.github.com>
2020-01-21 23:31:15 +0100

    Update cat.js

--- modules/cat.js
+++ modules/cat.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 var catApiUrl = 'https://api.thecatapi.com/v1/images/search';
 
-exports.Send = function(message) {
+exports.send = function(message) {
     request(catApiUrl, function (error, response, body) {
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
