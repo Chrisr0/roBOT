766bf6d732dcc3bdb49a9e5dcfb238601871176d
Chrisr0 <36136394+Chrisr0@users.noreply.github.com>
2020-01-21 22:33:28 +0100

    Update cat.js

--- modules/cat.js
+++ modules/cat.js
@@ -7,7 +7,7 @@ exports.getUrl = function() {
     request(catApiUrl, function (error, response, body) {
         console.error('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        url = JSON.parse(body)[0].url;
+        console.log('body:', body);
     });
 
     return url;
