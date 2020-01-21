25ba8aa76e180d605db6876221a8eac289312339
Chrisr0 <36136394+Chrisr0@users.noreply.github.com>
2020-01-21 22:31:50 +0100

    Update cat.js

--- modules/cat.js
+++ modules/cat.js
@@ -1,17 +1,14 @@
-const fetch = require("node-fetch");
+const request = require('request');
 
 var catApiUrl = 'https://api.thecatapi.com/v1/images/search';
 
 exports.getUrl = function() {
     var url;
-    var response = fetch(catApiUrl);
-
-    if(response.ok){
-        url = response.json()[0].url;
-    }else{
-        url = 'error';
-        console.log(response.statusText);
-    }
+    request(catApiUrl, function (error, response, body) {
+        console.error('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        url = JSON.parse(body)[0].url;
+    });
 
     return url;
 }
