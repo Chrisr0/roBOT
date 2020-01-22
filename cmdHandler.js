86d58f1cb5ebaa82ac9ff4d5699161aa9ce1b51a
Chrisr0 <diaxonex@gmail.com>
2020-01-22 03:35:13 +0100

    temporary patch for testing

--- cmdHandler.js
+++ cmdHandler.js
@@ -3,7 +3,7 @@ const Discord = require ('discord.js');
 const CatModule = require('./modules/cat.js');
 
 exports.exec = function (message){
-    var cmd = message.content.substring(prefix.length);
+    var cmd = message.content.substring(2); //prefix.length
     var args = cmd.split(" ");
     cmd = args[0];
     args.shift();
