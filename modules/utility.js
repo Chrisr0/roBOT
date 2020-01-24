55e345c02a2888e15ce24106f4ab1f95e4add072
Krzysztof Rurka <k_rurka@wp.pl>
2020-01-24 18:03:39 +0100

    Added response on autospoiler toogle

--- modules/utility.js
+++ modules/utility.js
@@ -6,8 +6,10 @@ exports.autoSpoiler = autoSpoiler;
 
 exports.toggle = function (message) {
     if (autoSpoiler.has(message.author.id)) {
+        message.reply("AutoSpoiler: OFF");
         autoSpoiler.delete(message.author.id);
     } else {
+        message.reply("AutoSpoiler: ON");
         autoSpoiler.add(message.author.id);
     }
 }
