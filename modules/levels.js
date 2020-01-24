869747888c22986c065701aa9bdcff9fa8eb7802
Krzysztof Rurka <k_rurka@wp.pl>
2020-01-24 21:46:24 +0100

    final test

--- modules/levels.js
+++ modules/levels.js
@@ -17,30 +17,34 @@ var getScore = "SELECT * FROM scores WHERE user = ? AND guild = ?";
 var setScore = "INSERT INTO scores (id,user,guild,exp,level) VALUES (?,?,?,?,?) ON DUPLICATE KEY UPDATE exp = ?, level = ?";
 
 exports.addLevel = function (message) {
-    //let score = getScore.get(message.author.id, message.guild.id);
-    let score = null;
-	if (!score) {
-		score = {
-			id: `${message.guild.id}-${message.author.id}`,
-			user: message.author.id,
-			guild: message.guild.id,
-			exp: 0,
-			level: 1
-		}
-	}
-	score.exp += Math.floor(Math.random() * (25 - 15 + 1) + 15);
-	let expNeeded = 5 * Math.pow(score.level) + 50 * score.level + 100;
+    pool.query(getScore, [message.author.id, message.guild.id], function (error, results, fields) {
+        if (error) {
+            return console.error(error.message);
+        }
+        let score = results[0];
+        if (!score) {
+            score = {
+                id: `${message.guild.id}-${message.author.id}`,
+                user: message.author.id,
+                guild: message.guild.id,
+                exp: 0,
+                level: 1
+            }
+        }
+        score.exp += Math.floor(Math.random() * (25 - 15 + 1) + 15);
+        let expNeeded = 5 * Math.pow(score.level) + 50 * score.level + 100;
 
-	if (score.exp >= expNeeded) {
-		score.exp -= expNeeded;
-		score.level++;
-		message.channel.send(`:gg: ${message.member} wbił/a ${score.level} poziom!`);
-	}
-	pool.query(setScore, [score.id, score.user, score.guild, score.exp, score.level, score.exp, score.level], function (error, results, fields) {
-	    if (error) {
-	        return console.error(error.message);
-	    }
-	});
+        if (score.exp >= expNeeded) {
+            score.exp -= expNeeded;
+            score.level++;
+            message.channel.send(`:gg: ${message.member} wbił/a ${score.level} poziom!`);
+        }
+        pool.query(setScore, [score.id, score.user, score.guild, score.exp, score.level, score.exp, score.level], function (error, results, fields) {
+            if (error) {
+                return console.error(error.message);
+            }
+        });
+    });
 }
 
 exports.getLevel = function (message) {
