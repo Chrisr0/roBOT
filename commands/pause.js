d37e825065bc6af07eb4a7bb5cee567555cb0beb
Chrisr0 <36136394+Chrisr0@users.noreply.github.com>
2020-03-31 21:42:33 +0200

    pause test

---
+++ commands/pause.js
@@ -0,0 +1,18 @@
+const ytdl = require('ytdl-core');
+
+module.exports = {
+    name: 'pause',
+    description: 'Pause music from youtube <BETA>!',
+    args: true,
+    usage: '<url>',
+    cooldown: 5,
+    async execute(message, args) {
+        console.log(message.member);
+        if (message.member.voiceChannel) {
+            const dispatcher = message.client.connection;
+            dispatcher.pause();
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    },
+};
