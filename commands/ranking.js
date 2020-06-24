a82921aa841d6961c29b62fc683e23a6fef3a6b9
Chrisr0 <36136394+Chrisr0@users.noreply.github.com>
2020-06-25 00:02:43 +0200

    fix

--- commands/ranking.js
+++ commands/ranking.js
@@ -19,6 +19,8 @@ module.exports = {
                 try{
                     member = await message.guild.fetchMember(user);
                 }
+                catch(e){
+                }
                 if (member){ 
                     let name = member.nickname || user.username;
                     ranking += `${i + 1}. ${name} Level: ${result.level}\n`;
