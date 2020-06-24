7615578d71eafd5eb0a524f4c1fa32032bb6b908
Chrisr0 <36136394+Chrisr0@users.noreply.github.com>
2020-06-24 23:32:49 +0200

    test

--- commands/ranking.js
+++ commands/ranking.js
@@ -14,7 +14,7 @@ module.exports = {
             let counter = 0;
             results.forEach(async(result) => {
                 let user = await message.client.fetchUser(result.user);
-                if(!user) return;
+                console.log(user);
                 let member = await message.guild.fetchMember(user);
                 let name = member.nickname || user.username;
                 ranking += `${counter + 1}. ${name} Level: ${result.level}\n`;
